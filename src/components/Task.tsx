bcd22c7ab3ce171c3a5bbfbe9404ce06497ca503
Oussama Moutafatin <moutafatin3@gmail.com>
2022-10-02 09:55:18 +0100

    tasks components

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,16 +1,41 @@
+import clsx from "clsx";
+import { HiPencil, HiTrash } from "react-icons/hi";
+import { TaskType } from "../pages/app";
+import IconButton from "./IconButton";
+
 type TaskProps = {
-  children: React.ReactNode;
+  task: TaskType;
 };
 
-export const Task = ({ children }: TaskProps) => {
+export const Task = ({ task }: TaskProps) => {
   return (
-    <li className="bg-white p-4 rounded-md">
+    <li className="bg-white p-4 rounded-md  flex items-center justify-between">
       <div className="flex items-center space-x-2">
-        <input type="checkbox" className="" />
+        <input
+          type="checkbox"
+          checked={task.isCompleted}
+          className="text-indigo-500 p-3 rounded-md transition-all duration-200 focus:ring-0"
+        />
         <div className="flex flex-col">
-          <span className="font-semibold">{children}</span>
-          <span className="text-sm">8:28 AM, 10/02/2022</span>
+          <span
+            className={clsx(
+              "font-semibold text-gray-600",
+              task.isCompleted && "line-through text-gray-400"
+            )}
+          >
+            {task.task}
+          </span>
+          <span className="text-sm text-gray-500">{task.createdAt}</span>
         </div>
+      </div>
+      {/* Actions */}
+      <div className="flex items-center space-x-2">
+        <IconButton>
+          <HiTrash className="text-gray-600" />
+        </IconButton>
+        <IconButton>
+          <HiPencil className="text-gray-600" />
+        </IconButton>
       </div>
     </li>
   );
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,17 @@
-import { Task } from "./Task";
+import { Task } from ".";
+import { TaskType } from "../pages/app";
 
-export const TaskList = () => {
+type TasksListProps = {
+  tasks: TaskType[];
+};
+
+export const TaskList = ({ tasks }: TasksListProps) => {
   return (
     <div className="mt-5">
-      <ul className="bg-gray-400 rounded-md p-3 space-y-4">
-        <Task>Task 1</Task>
-        <Task>Task 2</Task>
+      <ul className="bg-gray-300 rounded-xl p-6 space-y-4">
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
       </ul>
     </div>
   );
