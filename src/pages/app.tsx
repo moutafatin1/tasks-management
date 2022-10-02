48193498a0fa8c8e64818ce951b271dea4ad5714
Oussama Moutafatin <moutafatin3@gmail.com>
2022-10-02 09:54:59 +0100

    tasks main page

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,21 +1,39 @@
-import { Button, TaskList } from "../components";
+import { TaskList } from "../components";
+import HeaderActions from "../components/HeaderActions";
+import { formatAMPM } from "../utils";
+
+const tasks: TaskType[] = [
+  {
+    id: 1,
+    task: "This is the first task",
+    isCompleted: true,
+    createdAt: `${formatAMPM(new Date())} ${new Date().toLocaleDateString()}`,
+  },
+  {
+    id: 2,
+    task: "This is the second task",
+    isCompleted: false,
+    createdAt: `${formatAMPM(new Date())} ${new Date().toLocaleDateString()}`,
+  },
+];
+
+export type TaskType = {
+  id: number;
+  task: string;
+  isCompleted: boolean;
+  createdAt: string;
+};
 
 const AppPage = () => {
   return (
-    <main className="h-screen  bg-slate-300">
+    <main className="h-screen  bg-slate-200">
       {/* Title */}
       <div className="container px-4 mx-auto">
-        <h1 className="text-center font-bold  text-6xl p-10">Tasks List</h1>
-        {/* Add task / filter actions */}
-        <div className="flex items-center justify-between">
-          <Button>Add Task</Button>
-          <select name="" id="">
-            <option value="completed">Completed</option>
-            <option value="incomplete">Incomplete</option>
-          </select>
-        </div>
-        {/* Tasks List */}
-        <TaskList />
+        <h1 className="text-center font-bold  text-6xl p-10 text-gray-600">
+          Tasks List
+        </h1>
+        <HeaderActions />
+        <TaskList tasks={tasks} />
       </div>
     </main>
   );
