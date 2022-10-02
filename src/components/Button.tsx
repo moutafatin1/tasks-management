f12da87f17f307a550b11be6c9584eecddae49a2
Oussama Moutafatin <moutafatin3@gmail.com>
2022-10-02 10:44:44 +0100

    button accept more props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 type ButtonProps = {
   children: React.ReactNode;
-};
-export const Button = ({ children }: ButtonProps) => {
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = ({ children, ...props }: ButtonProps) => {
   return (
-    <button className="px-6 py-3 bg-indigo-500 text-slate-100 font-bold rounded-md transition-all duration-75  hover:bg-indigo-600 active:scale-95">
+    <button
+      {...props}
+      className="px-6 py-3 bg-indigo-500 text-slate-100 font-bold rounded-md transition-all duration-75  hover:bg-indigo-600 active:scale-95"
+    >
       {children}
     </button>
   );
