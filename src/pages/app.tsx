3375b0e81c2827272fe32d25567433fd1e58f264
Oussama Moutafatin <moutafatin3@gmail.com>
2022-10-05 08:58:37 +0100

    add github domain for user profile

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,6 +1,8 @@
 import { GetServerSideProps } from "next";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import { useReducer, useState } from "react";
+import { HiOutlineLogout } from "react-icons/hi";
 import { TaskList } from "../components";
 import HeaderActions from "../components/HeaderActions";
 import TaskModal from "../components/TaskModal";
@@ -28,16 +30,51 @@ const AppPage = () => {
   });
 
   const { data: session } = useSession();
-  console.log("🚀 ~ file: app.tsx ~ line 29 ~ AppPage ~ session", session);
+
+  const firstName = () => {
+    if (session?.user?.name) {
+      return session.user.name.split(" ")[0];
+    }
+    return "";
+  };
+
+  const lastName = () => {
+    if (session?.user?.name) {
+      return session.user.name.split(" ")[1];
+    }
+    return "";
+  };
 
   return (
     <main className="h-screen  bg-slate-200">
       {/* Title */}
 
       <div className="container px-4 mx-auto">
-        <h1 className="text-center font-bold  text-6xl p-10 text-gray-600">
-          Tasks List
-        </h1>
+        <div className="flex items-center justify-between py-10">
+          <h1 className="text-center font-bold  text-6xl  text-gray-600">
+            Tasks List
+          </h1>
+          <div className="flex items-center">
+            <div className="flex flex-col items-center border-r border-gray-400 pr-3 ">
+              <div className="relative h-20 w-20 ">
+                <Image
+                  className="absolute rounded-full"
+                  src={session?.user?.image ?? ""}
+                  alt={session?.user?.name ?? "user"}
+                  layout="fill"
+                />
+              </div>
+              <div className="flex flex-col items-center font-bold text-gray-600">
+                <span>{firstName()}</span>
+                <span>{lastName()}</span>
+              </div>
+            </div>
+            {/* Logout  */}
+            <button onClick={() => signOut()} className=" rounded-md mx-2">
+              <HiOutlineLogout className="text-3xl transition-colors hover:text-red-500" />
+            </button>
+          </div>
+        </div>
         <HeaderActions
           setOpenModal={setOpenModal}
           openModal={openModal.isOpen}
